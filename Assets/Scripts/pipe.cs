b37af01fdd74be451add72723a62b90ff530e69a
Andrew Huth <andrew@huth.me>
2021-10-23 11:22:54 -0700

    Add a pipe thing

---
+++ Assets/Scripts/pipe.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Move to the left.
+        Rigidbody2D pipe = GetComponent<Rigidbody2D>();
+        pipe.velocity = new Vector2(-7, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
